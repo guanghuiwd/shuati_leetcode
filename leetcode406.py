c1f3dd5ca8561b5d00a2a00562153cb23000a876
guanghuiwd <42063491+guanghuiwd@users.noreply.github.com>
2019-09-20 15:01:30 +0800

    Update leetcode406.py

--- leetcode406.py
+++ leetcode406.py
@@ -1,18 +1,16 @@
-#
-
-#先插高的，后插矮的，即使后插的插到前面也不会有影响，因为矮。
-
+"""
+假设有打乱顺序的一群人站成一个队列。 每个人由一个整数对(h, k)表示，其中h是这个人的身高，k是排在这个人前面且身高大于或等于h的人数。 编写一个算法来重建这个队列。
+"""
 if __name__ == '__main__':
-    people = [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]
-
-    people.sort(key=lambda x: [-x[0], x[1]])
+    people = [[9, 0], [7, 0], [1, 9], [3, 0], [2, 7], [5, 3], [6, 0], [3, 4], [6, 2], [5, 2]]
+    if not people:
+        print([])
+    people.sort(key=lambda x: x[1])  # 按面前几个高个子排序
+    people.sort(key=lambda x: x[0], reverse=True)  # 按身高排序
     res = []
-    for p in people:
-        res.insert(p[1], p)
+    for i in people:
+        res.insert(i[1], i)  # 先插高的，后插矮的，即使后插的插到前面也不会有影响，因为矮。
     print(res)
-
-
-
 
 # 有问题的代码16/37 ac
 # if __name__ == '__main__':
